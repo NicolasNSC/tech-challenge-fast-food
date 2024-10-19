bac53e6944bb3ec887975fb6cef0a4e5c15abb56
Gabriel Oliveira Lima <gabriel.lima@artesanalinvestimentos.com.br>
2024-10-19 16:50:25 -0300

    testing push

--- src/domain/entities/Client.ts
+++ src/domain/entities/Client.ts
@@ -10,4 +10,20 @@ export class Client {
     this.email = email;
     this.documentNumber = documentNumber;
   }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public getDocumentNumber(): string {
+    return this.documentNumber;
+  }
 }
