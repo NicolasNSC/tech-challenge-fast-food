759b3057dfb2b41e41d7afba5eff36b5519b56fe
Gabriel Oliveira Lima <gabriel.lima@artesanalinvestimentos.com.br>
2024-10-19 18:03:38 -0300

    added client tests

--- src/domain/entities/Client.ts
+++ src/domain/entities/Client.ts
@@ -1,25 +1,42 @@
-export class Client {
-  public id: string;
+import { Entity } from "../../shared/domain/Entity";
+import { Uuid } from "../../shared/domain/value-objects/uuid.value-object";
+import { ValueObject } from "../../shared/domain/ValueObject";
+
+export type ClientProps = {
+  clientId?: Uuid;
+  name: string;
+  email: string;
+  documentNumber?: string;
+  createdAt?: Date;
+};
+
+export class Client extends Entity{
+  public clientId: Uuid;
   public name: string;
   public email: string;
-  public documentNumber: string;
+  public documentNumber: string | null;
+  public createdAt: Date;
 
-  constructor(id: string, name: string, email: string, documentNumber: string) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.documentNumber = documentNumber;
+  constructor(props: ClientProps) {
+    super();
+    this.clientId = props.clientId ?? new Uuid();
+    this.name = props.name;
+    this.email = props.email;
+    this.documentNumber = props.documentNumber || null;
+    this.createdAt = props.createdAt || new Date();
   }
 
-  public getId(): string {
-    return this.id;
+  get entityId(): ValueObject {
+    return this.clientId;
   }
 
-  public getName(): string {
-    return this.name;
-  }
-
-  public getEmail(): string {
-    return this.email;
+  toJSON() {
+    return {
+      id: this.clientId.id,
+      name: this.name,
+      email: this.email,
+      documentNumber: this.documentNumber,
+      createdAt: this.createdAt,
+    }
   }
 }
--- src/domain/entities/__tests__/Client.spec.ts
+++ src/domain/entities/__tests__/Client.spec.ts
@@ -1,15 +1,50 @@
+import { Uuid } from 'src/shared/domain/value-objects/uuid.value-object';
 import { Client } from '../Client';
 
 describe('Client', () => {
-  test('should instantiate the class', () => {
-    const client = new Client(
-      '1',
-      'John Doe',
-      '',
-      '12345678900'
-    );
+  test('should be defined', () => {
+    const client = new Client({
+      name: 'Jhon Doe',
+      email: '',
+    });
 
-    expect(client).toBeInstanceOf(Client);
+    expect(client).toBeDefined();
+  })
+
+  describe('Constructor of Client', () => {
+    test('constructor', () => {
+      let client = new Client({
+        name: 'Jhon Doe',
+        email: 'jhon@doe.com',
+      })
+
+      expect(client.clientId).toBeInstanceOf(Uuid);
+      expect(client.name).toBe('Jhon Doe');
+      expect(client.email).toBe('jhon@doe.com');
+      expect(client.documentNumber).toBe(null);
+      expect(client.createdAt).toBeInstanceOf(Date);
+
+      client = new Client({
+        name: 'Jhon Doe',
+        email: 'jhon@doe.com',
+        documentNumber: '12345678900',
+      })
+
+      expect(client.clientId).toBeInstanceOf(Uuid);
+      expect(client.name).toBe('Jhon Doe');
+      expect(client.email).toBe('jhon@doe.com');
+      expect(client.documentNumber).toBe('12345678900');
+      expect(client.createdAt).toBeInstanceOf(Date);
+    })
+
+    describe("clientId field", () => {
+      const arrange = [{ id: null }, { id: undefined }, { id: new Uuid() }];
+  
+      test.each(arrange)("should be %j", (props) => {
+        const category = new Client(props as any);
+        expect(category.clientId).toBeInstanceOf(Uuid);
+      });
+    });
   })
 });
 
