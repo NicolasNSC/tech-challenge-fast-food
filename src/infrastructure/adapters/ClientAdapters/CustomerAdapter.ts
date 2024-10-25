328f0459c4c322b21f99a9107cfa98fa97e960e1
Gabriel Oliveira Lima <gabriel.lima@artesanalinvestimentos.com.br>
2024-10-25 11:08:57 -0300

    created customer adapter to remove the responsability from Customer Entity class

---
+++ src/infrastructure/adapters/ClientAdapters/CustomerAdapter.ts
@@ -0,0 +1,26 @@
+import { Customer } from "src/domain/entities/Customer/Customer";
+import { CustomerProps } from "src/domain/entities/Customer/types";
+import { Cpf } from "src/shared/domain/value-objects/cpf.value-object";
+import { Uuid } from "src/shared/domain/value-objects/uuid.value-object";
+
+export class CustomerAdapter {
+  static toDomain(dto: CustomerProps): Customer {
+    return Customer.create({
+      customerId: new Uuid(),
+      email: dto.email,
+      name: dto.name,
+      cpf: dto.cpf ? new Cpf(dto.cpf.getValue()) : null,
+      createdAt: new Date(),
+    })
+  }
+
+  static toDTO(customer: Customer): CustomerProps {
+    return {
+      customerId: customer.customerId,
+      name: customer.name,
+      email: customer.email,
+      cpf: customer.cpf,
+      createdAt: customer.createdAt,
+    };
+  }
+}
